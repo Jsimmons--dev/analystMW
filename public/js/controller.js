ec7a1885d1baa0a5d9cc474daa18f5a7d72f666d
jsimmons--dev <jsimmons@udel.edu>
2015-09-11 17:01:23 -0400

    added resizable svg

--- public/js/controller.js
+++ public/js/controller.js
@@ -2,7 +2,7 @@ var boilerplate = angular.module('boilerplate', ["ngRoute", "firebase", "ui.boot
 
 
 //routing happens here
-socialMediaApp.config(['$routeProvider', function ($routeProvider) {
+boilerplate.config(['$routeProvider', function ($routeProvider) {
 
     $routeProvider
         .when('/landing', {
@@ -15,7 +15,52 @@ socialMediaApp.config(['$routeProvider', function ($routeProvider) {
 
 }]);
 
-socialMediaApp.controller('landingController', function ($scope, $firebaseObject, profileService, $location, $route) {
-    var base = new Firebase("https://allofus.firebaseio.com/");
+boilerplate.service('vizService',function(){
+
+		var service = this;
+	    this.chartDivID = "chart";
+
+	    this.chartWidth = $("#"+this.chartDivID).width();
+		this.chartHeight = $(window).height()/1.3;
+
+		this.svg = function(selector,spec){
+			d3.select(selector).selectAll("svg").remove();
+			return d3.select(selector).append("svg")	
+				.attr({
+					"width":service.chartWidth,
+					"height":service.chartHeight
+				});
+		}
+
+		this.drawRect = function(d3sel){
+			d3sel.append("rect")
+				.attr({
+					"width":d3sel.attr("width"),
+					"height":d3sel.attr("height")
+				});
+		}
+
+});
+
+boilerplate.controller('landingController', function ($scope, $firebaseObject, $location, $route,vizService,$window) {
+	var baseURL = "analyzetester";
+    var base = new Firebase("https://"+baseURL+".firebaseio.com/");
+	var info = $firebaseObject(base.child("info"));
+	
+	info.$bindTo($scope, "info");
+
+	var chart = vizService.svg("#"+vizService.chartDivID,{width:vizService.chartWidth,height:500})
+
+	vizService.drawRect(chart);
+
+	var w = angular.element($window);
+
+	w.bind('resize',function () {
+		chart.attr({
+				"width":$("#"+vizService.chartDivID).width(),
+				"height":$(window).height()/2,
+		});
+	});
+
 });
 
