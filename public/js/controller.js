07ddc7ef87ef4ae735fc51c61695fbb2e596cec3
jsimmons--dev <jsimmons@udel.edu>
2015-09-16 00:25:03 -0400

    added multiple malware categories

--- public/js/controller.js
+++ public/js/controller.js
@@ -35,7 +35,7 @@ boilerplate.service('vizService', function () {
             .append("svg")
             .attr({
                 "width": service.chartWidth,
-                "height": service.chartHeight
+                "height": service.chartHeight,
             });
     }
 
@@ -44,6 +44,7 @@ boilerplate.service('vizService', function () {
             .attr({
                 "width": d3sel.attr("width"),
                 "height": d3sel.attr("height"),
+                "fill": "#222222",
             });
     }
 
@@ -55,13 +56,23 @@ boilerplate.service('malwareService', ['url', function (url) {
     var base = new Firebase("https://" + url + ".firebaseio.com/");
 
     var malwareUrl = "malware";
+	base.child(malwareUrl+"/name").set("name");
+	base.child(malwareUrl+"/intensity").set(1);
 
-    this.addMalware = function (name, value) {
-        base.child(malwareUrl)
-            .push({
-                name: name,
-                intensity: value
-            });
+    this.addMalware = function (name, value, category) {
+        base.child(malwareUrl+"/children/"+category)
+            .set({
+                "name": "name",
+                intensity: 1,
+				"category":category
+            })
+		for(var i = 0; i<15;i++){
+        base.child(malwareUrl+"/children/"+category+"/children/"+i)
+            .set({
+                "name": "name",
+                "intensity": Math.floor(Math.random()*1000),
+            })
+		}
     }
 
 
@@ -70,9 +81,13 @@ boilerplate.service('malwareService', ['url', function (url) {
             var randomMD5 = (Math.floor((Math.random() + 1) * 1000000000))
                 .toString(16);
             var randomIntensity = Math.floor(Math.random() * 1000);
-            this.addMalware(randomMD5, randomIntensity);
+            this.addMalware(randomMD5, randomIntensity,i);
         }
     }
+
+	base.child("malware").remove();
+	this.loadSampleMW(10);
+
 }]);
 
 boilerplate.controller('landingController', function ($scope, $firebaseObject, $location, $route, vizService, $window, malwareService) {
@@ -82,11 +97,12 @@ boilerplate.controller('landingController', function ($scope, $firebaseObject, $
 
     info.$bindTo($scope, "info");
 
-
     var chart = vizService.svg("#" + vizService.chartDivID, {
         width: vizService.chartWidth,
         height: 500
     })
+
+    vizService.drawRect(chart)
 
     var w = angular.element($window);
 
@@ -99,7 +115,6 @@ boilerplate.controller('landingController', function ($scope, $firebaseObject, $
         });
     });
 
-
     malwarePack = d3.layout.pack()
         .size([chart.attr("width") * .75, chart.attr("height") * .75])
         .value(function (d) {
@@ -109,6 +124,13 @@ boilerplate.controller('landingController', function ($scope, $firebaseObject, $
             return d.children;
         })
         .padding(4)
+        .sort(function (d) {
+            if (Math.floor(Math.random() * 10) > 5) {
+                return -1;
+            } else {
+                return 1;
+            }
+        })
 
     var root = {
         "name": "root"
@@ -124,7 +146,6 @@ boilerplate.controller('landingController', function ($scope, $firebaseObject, $
 
         this.malware && this.malware.remove();
 
-
         var nodes = malwarePack.nodes(root);
 
         this.malware = chart.datum(root)
@@ -137,16 +158,16 @@ boilerplate.controller('landingController', function ($scope, $firebaseObject, $
             })
             .attr("transform", function (d) {
                 return "translate(" +
-                    (d.x + (chart.attr("width")*.125)) +
-				   	"," +
-                    (d.y + chart.attr("height")*.125) +
+                    (d.x + (chart.attr("width") * .125)) +
+                    "," +
+                    (d.y + chart.attr("height") * .125) +
                     ")";
             })
 
+		var hslAng = 0;
 
         var color = d3.scale.linear()
             .domain([0, 1000])
-            .range([d3.rgb("green"), d3.rgb("red")]);
 
         malware.append("title")
             .text(function (d) {
@@ -161,54 +182,58 @@ boilerplate.controller('landingController', function ($scope, $firebaseObject, $
                 "opacity": function (d) {
                     return d.children ? .25 : 1
                 },
-                "fill": function (d) {
+                "fill": function (d,i) {
+						console.log(d);
+					if(d.category !== undefined)
+					color.range([d3.hsl(d.category*30,0,0),d3.hsl(d.category*30,1,.6)])
                     return d.children ? "none" : color(d.intensity);
                 },
-                "stroke": "black",
+                "stroke": "#dddddd",
                 "stroke-width": function (d) {
-                    return  d.children ? 0 : 1.1
+                    return d.children ? 0 : 1.1
                 }
             });
     }
 
 
-	var analystRadius = Math.min(chart.attr("width"),chart.attr("height")) / 2;
-	var	analystData = [
-		{"name":"John",worth:100},
-		{"name":"Corey",worth:1000},
-		{"name":"John",worth:423},
-		{"name":"Teague",worth:1000}]
+    var analystRadius = Math.min(chart.attr("width"), chart.attr("height")) / 2;
+    var analystData = [{ "name": "John", worth: 1000 }, { "name": "Corey", worth: 1000 }, { "name": "John", worth: 1000 }, { "name": "Teague", worth: 1000 }, { "name": "Shane", worth: 1000 }, { "name": "Eric", worth: 1000 }, { "name": "Keith", worth: 1000 }, { "name": "Jacob", worth: 1000 },{ "name": "John", worth: 1000 }, { "name": "Corey", worth: 1000 }, { "name": "John", worth: 1000 }, { "name": "Teague", worth: 1000 }, { "name": "Shane", worth: 1000 }, { "name": "Eric", worth: 1000 }, { "name": "Keith", worth: 1000 }, { "name": "Jacob", worth: 1000 },{ "name": "John", worth: 1000 }, { "name": "Corey", worth: 1000 }, { "name": "John", worth: 1000 }, { "name": "Teague", worth: 1000 }, { "name": "Shane", worth: 1000 }, { "name": "Eric", worth: 1000 }, { "name": "Keith", worth: 1000 }, { "name": "Jacob", worth: 1000 },{ "name": "John", worth: 1000 }, { "name": "Corey", worth: 1000 }, { "name": "John", worth: 1000 }, { "name": "Teague", worth: 1000 }, { "name": "Shane", worth: 1000 }, { "name": "Eric", worth: 1000 }, { "name": "Keith", worth: 1000 }, { "name": "Jacob", worth: 1000 },{ "name": "John", worth: 1000 }, { "name": "Corey", worth: 1000 }, { "name": "John", worth: 1000 }, { "name": "Teague", worth: 1000 }, { "name": "Shane", worth: 1000 }, { "name": "Eric", worth: 1000 }, { "name": "Keith", worth: 1000 }, { "name": "Jacob", worth: 1000 },{ "name": "John", worth: 1000 }, { "name": "Corey", worth: 1000 }, { "name": "John", worth: 1000 }, { "name": "Teague", worth: 1000 }, { "name": "Shane", worth: 1000 }, { "name": "Eric", worth: 1000 }, { "name": "Keith", worth: 1000 }, { "name": "Jacob", worth: 1000 }];
 
 
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
-	.domain([0,analystData.length-1])
+    var color = d3.scale.ordinal()
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"])
+        .domain([0, analystData.length - 1])
 
-	var arc = d3.svg.arc()
-			.outerRadius(analystRadius)
-			.innerRadius(analystRadius - 25)
+    var arc = d3.svg.arc()
+        .outerRadius(analystRadius - 5)
+        .innerRadius(analystRadius - 30)
 
-	var pie = d3.layout.pie()
-		.sort(null)
-		.value(function(d) {return d.worth})
-			.padAngle(.05)
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function (d) {
+            return d.worth
+        })
+        .padAngle(.05)
 
-			
-	var analystRing = chart.selectAll(".arc")
-		.data(pie(analystData))
-		.enter().append("g")
-		.attr({
-				"class":"arc",
-				"transform":"translate("+chart.attr("width")/2+","+chart.attr("height")/2+")"
-		})
 
-	analystRing.append("path")
-		.attr("d",arc)
-		.style({
-				"fill":function(d,i) {return color(i)},
-				"stroke":"black",
-				"stroke-width":1
-				})
+    var analystRing = chart.selectAll(".arc")
+        .data(pie(analystData))
+        .enter()
+        .append("g")
+        .attr({
+            "class": "arc",
+            "transform": "translate(" + chart.attr("width") / 2 + "," + chart.attr("height") / 2 + ")"
+        })
+
+    analystRing.append("path")
+        .attr("d", arc)
+        .style({
+            "fill": function (d, i) {
+                return color(i)
+            },
+            "stroke": "#dddddd",
+            "stroke-width": 1
+        })
 
     $(document)
         .on("malware_updated", function () {
@@ -224,8 +249,9 @@ var color = d3.scale.ordinal()
         });
 
     base.child('malware')
-        .on('child_added', function (snap) {
-            root.children.push(snap.val());
+        .on('value', function (snap) {
+			console.log(snap.val());
+            root = snap.val();
             throttleTrigger();
             console.log("how many total?");
         });
